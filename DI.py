edae2583692610625d96f8b8d1fc096bd333c23e
衍 <jixiong.liu@qq.com>
2018-12-09 15:05:16 +0100

    Update DI.py

--- DI.py
+++ DI.py
@@ -10,19 +10,17 @@ import keras.layers.convolutional as conv
 import keras.models as models
 import keras.utils.np_utils as kutils
 
-# The competition datafiles are in the directory ../input
-# Read competition data files:
 train = pd.read_csv("../input/train.csv").values
 test  = pd.read_csv("../input/test.csv").values
 
-nb_epoch = 1 # Change to 100
+nb_epoch = 1
 
 batch_size = 128
 img_rows, img_cols = 28, 28
 
-nb_filters_1 = 32 # 64
-nb_filters_2 = 64 # 128
-nb_filters_3 = 128 # 256
+nb_filters_1 = 32 # nfro 64
+nb_filters_2 = 64 # for 128
+nb_filters_3 = 128 # for 256
 nb_conv = 3
 
 trainX = train[:, 1:].reshape(train.shape[0], img_rows, img_cols, 1)
